d7b5f649b086dff905d381fb174427042d685bb8
SREEDHAR <sree@SREEDHARs-MacBook-Pro.local>
2020-03-28 09:27:51 -0400

    .

---
+++ test.py
@@ -0,0 +1,28 @@
+
+schoolClass = {}
+
+while True:
+    name = input("Enter the student's name (or type exit to stop): ")
+    if name == 'exit':
+        break
+
+    score = int(input("Enter the student's score (0-10): "))
+
+    if name in schoolClass:
+        schoolClass[name] += (score,)
+        #print(schoolClass)
+    else:
+        schoolClass[name] = (score,)
+        #print(schoolClass)
+
+print(sorted(schoolClass.keys()))
+
+'''
+for name in sorted(schoolClass.keys()):
+    sum = 0
+    counter = 0
+    for score in schoolClass[name]:
+        sum += score
+        counter += 1
+    print(name, ":", sum / counter)
+'''
---
+++ test2.py
@@ -0,0 +1,5 @@
+dct = {"abc": (1,2,3)}
+
+dct["abc"]+=(4,)
+
+print(dct["abc"])
